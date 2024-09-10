62cfb23ee2109a06ccecfb34c2862f2bb24e2090
ombiradar07 <omkarsbiradar23@gmail.com>
2024-09-10 15:46:19 +0530

    Added HealthController to test the security

---
+++ src/main/java/com/servicebuddy/Controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.servicebuddy.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping("/health")
+     public ResponseEntity<String> health() {
+         return ResponseEntity.ok("Service is up and running");
+    }
+}
